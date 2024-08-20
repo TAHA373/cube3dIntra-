aa8d94c117e4ee0eb86dd32849ab48fca6504bd2
Taha Kannane <tkannane@e2r9p15.1337.ma>
2024-08-20 17:55:43 +0100

    add MLX42

--- mandatory/cub3d.h
+++ mandatory/cub3d.h
@@ -6,14 +6,14 @@
 /*   By: tkannane <tkannane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 16:16:43 by soel-bou          #+#    #+#             */
-/*   Updated: 2024/08/20 17:51:30 by tkannane         ###   ########.fr       */
+/*   Updated: 2024/08/20 17:54:38 by tkannane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "../MLX42/MLX42.h"
+# include "../MLXlib42/MLX42.h"
 # include "./parsing/get_next_line.h"
 # include "../libft/libft.h"
 # include <unistd.h>
